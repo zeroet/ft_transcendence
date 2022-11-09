2f11f07c84138d418258e3e8a6c5968b9132355b
hyungyoo <hjyoo901112@gmail.com>
2022-11-09 05:11:17 +0100

    udpate for docker-compose

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 make : docker-compose
 make fclean : container, volume, network, image 모두삭제
 make re: fclean all
+
+# docker volume (postgresql)
+이거는 docker volume inspect 로 찾아서 보시면 어디에 마운트되는지나옵니다!
--- backend/dist/users/users.service.js
+++ backend/dist/users/users.service.js
@@ -11,7 +11,6 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
-var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsersService = void 0;
 const common_1 = require("@nestjs/common");
@@ -29,7 +28,7 @@ let UsersService = class UsersService {
 UsersService = __decorate([
     (0, common_1.Injectable)(),
     __param(0, (0, typeorm_1.InjectRepository)(user_entity_1.User)),
-    __metadata("design:paramtypes", [typeof (_a = typeof typeorm_2.Repository !== "undefined" && typeorm_2.Repository) === "function" ? _a : Object])
+    __metadata("design:paramtypes", [typeorm_2.Repository])
 ], UsersService);
 exports.UsersService = UsersService;
 //# sourceMappingURL=users.service.js.map
