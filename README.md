1d3fbb7d1bf1156f2c7a7b1a27396006197c8b0e
hyungyoo <hjyoo901112@gmail.com>
2022-11-09 01:24:17 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ft_transcendence
+make : docker-compose
+make fclean : container, volume, network, image 모두삭제
+make re: fclean all
---
+++ front/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  const [value, setValue] = useState<number>(0);
+  const onClickEx = () => {
+    setValue((curr) => curr + 1);
+  };
+  return (
+    <div className={styles.container}>
+      <h2>number : {value}</h2>
+      <button onClick={onClickEx}>+</button>
+    </div>
+  );
+}
