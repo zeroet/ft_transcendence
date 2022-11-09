dcc18b185b6dc97a357667a1febca625c260bed8
hyungyoo <hjyoo901112@gmail.com>
2022-11-09 09:18:15 +0100

    로드밸런싱을 일단 주석처리했음. 프록시서버 우선으로 시작했음. 아직도 프론트에서 백으로 요청이안감

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,6 +1,7 @@
+import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -8,6 +9,15 @@ export default function Home() {
   const onClickEx = () => {
     setValue((curr) => curr + 1);
   };
+  const getData = async () => {
+    const data = axios
+      .get("http://localhost/api")
+      .then((res) => console.log(res))
+      .catch((res) => console.log(res));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
     <div className={styles.container}>
       <h2>gooooooooooooood !!!!!: {value}</h2>
