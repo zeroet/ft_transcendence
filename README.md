e7e0388266be89057bce7071dcbc67dc4730df05
hyungyoo <hjyoo901112@gmail.com>
2022-11-14 06:00:02 +0100

    콜스 완료

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ make re: fclean all
 
 # docker volume (postgresql)
 이거는 docker volume inspect 로 찾아서 보시면 어디에 마운트되는지나옵니다!
+
+
+# 프론트 개발
+front 폴더에 nginx폴더있음
+localhost:8000 으로 들어가면 프론트나옴
+
+# 백엔드
+backend에 nestjs 폴더있음
+localhost:8080 으로 
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -2,26 +2,20 @@ import axios from "axios";
 import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 import styles from "../styles/Home.module.css";
 
+const fetcher = async (url : string) => {
+  return axios.get(url).then(res => res.data).catch(res => res.data);
+}
+
 export default function Home() {
-  const [value, setValue] = useState<number>(0);
-  const onClickEx = () => {
-    setValue((curr) => curr + 1);
-  };
-  const getData = async () => {
-    const data = axios
-      .get("http://localhost:8080/test")
-      .then((res) => console.log(res))
-      .catch((res) => console.log(res));
-  };
-  useEffect(() => {
-    getData();
-  }, []);
+  const {data, error, isValidating} = useSWR('/api/test', fetcher)
+  
+  if (error) return ;
   return (
     <div className={styles.container}>
-      <h2>gooooooooooooood !!!!!: {value}</h2>
-      <button onClick={onClickEx}>+</button>
+      <h1>{!isValidating && data}</h1>
     </div>
   );
 }
