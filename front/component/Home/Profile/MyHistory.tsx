575b8df8fea6a750b92c48a80157ab3eff13b9c8
hyungyoo <hjyoo901112@gmail.com>
2022-11-15 03:50:46 +0100

    last 5 match history 컴포넌트

--- front/component/Home/Profile/MyHistory.tsx
+++ front/component/Home/Profile/MyHistory.tsx
@@ -1,11 +1,25 @@
+import EachHistory from "./EachHistory";
+
+// interface infoOfHistory {
+//   winOrLoss : string,
+//   firstPlayer : string,
+//   secondPlayer : string,
+//   point: string,
+// }
+
 const MyHistory = () => {
+  // EachHistoty -> 5개만 얻어와서 map으로 출력
   return (
     <div>
       <div className="dummy"></div>
       <div className="history">
         <h1>LAST 5 MATCHES HISTORY</h1>
-        <hr className="titleOfHistory"/>
-        <p>gh</p>
+        <hr className="titleOfHistory" />
+        {<EachHistory winOrLoss={'win'} firstPlayer={`hyungyoo`} secondPlayer={'cjung-mo'} point={"5:4"}  />}
+        {<EachHistory winOrLoss={'loss'} firstPlayer={`cjung-mo`} secondPlayer={'hyungyoo'} point={"4:1"}  />}
+        {<EachHistory winOrLoss={'win'} firstPlayer={`hyungyoo`} secondPlayer={'cjung-mo'} point={"5:4"}  />}
+        {<EachHistory winOrLoss={'loss'} firstPlayer={`cjung-mo`} secondPlayer={'hyungyoo'} point={"4:1"}  />}
+        {<EachHistory winOrLoss={'win'} firstPlayer={`hyungyoo`} secondPlayer={'cjung-mo'} point={"5:4"}  />}
       </div>
       <style jsx>{`
         div {
@@ -14,19 +28,19 @@ const MyHistory = () => {
         }
 
         h1 {
-            width : 100%;
+          width: 100%;
         }
 
         .titleOfHistory {
-            border-top: 2px solid brown;
+          border-top: 2px solid brown;
         }
 
         hr {
-            border-top: 1px solid;
-            width : 90%;
-            margin: auto;
-            margin-top : 0px;
-            margin-bottom: 0px;
+          border-top: 1px solid;
+          width: 90%;
+          margin: auto;
+          margin-top: 0px;
+          margin-bottom: 20px;
         }
 
         .history {
