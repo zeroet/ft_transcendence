e63647aa4054bfb224044b05511f4eac430ca56a
jmcheon <developer.cheon@gmail.com>
2022-11-16 20:52:58 +0100

    remove: dist directory in backend

--- backend/dist/auth/strategies/refresh.strategy.d.ts
+++
@@ -1,8 +0,0 @@
-import { Strategy } from 'passport-jwt';
-import { Request } from 'express';
-declare const RefreshStrategy_base: new (...args: any[]) => Strategy;
-export declare class RefreshStrategy extends RefreshStrategy_base {
-    constructor();
-    validate(req: Request, payload: any): Promise<any>;
-}
-export {};
--- backend/dist/test/test.entity.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class testEntity {
-    id: number;
-}
