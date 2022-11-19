98cd23d2c84d7abc0d0de8ce03bd3be562aa8773
jmcheon <developer.cheon@gmail.com>
2022-11-19 02:18:15 +0100

    add: unauthorized jwt refresh token exception

--- backend/src/auth/strategies/jwt.refresh.strategy.ts
+++ backend/src/auth/strategies/jwt.refresh.strategy.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
@@ -22,6 +22,7 @@ export class JwtRefreshStrategy extends PassportStrategy(Strategy, 'refresh') {
   async validate(payload: any) {
     console.log('validate func in jwt.refresh');
     const user = await this.userService.getUserById(payload.id);
+    if (!user) throw new UnauthorizedException('Unauthorized User');
     return user;
   }
 }
