858b4f2b1ea6c0dbdc5e480171fc9562725c07ac
hyungyoo <hjyoo901112@gmail.com>
2022-11-21 19:39:43 +0100

    update for loading animation

--- front/component/Game/GameBody.tsx
+++ front/component/Game/GameBody.tsx
@@ -1,20 +1,33 @@
-import { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 import styles from "../../styles/LayoutBox.module.css";
 
 export default function GameBody() {
   const [waitModal, setWaitModal] = useState(false);
 
-  const socket = io('/api/test');
+  const onClickWaitModal = useCallback(
+    (e: React.MouseEvent<HTMLImageElement>) => {
+      e.preventDefault();
+      e.stopPropagation();
+      setWaitModal((curr) => !curr);
+    },
+    []
+  );
 
-  console.log(socket);
-  const onClickWaitModal = (e: React.MouseEvent<HTMLImageElement>) => {
+  const onClickCancle = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
     e.stopPropagation();
     setWaitModal((curr) => !curr);
-  };
+    // socket 취소
+  }, []);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    // const socket = io('/api/test');
+    // console.log(socket);
+    return () => {
+      // socket off
+    };
+  }, []);
   return (
     <div className={styles.box}>
       {!waitModal ? (
@@ -26,13 +39,96 @@ export default function GameBody() {
           height={90}
         />
       ) : (
-        <div>haha</div>
+        <div>
+          <div className="ring">Loading</div>
+          <div onClick={onClickCancle} className="cancle">
+            Cancle
+          </div>
+        </div>
       )}
       <style jsx>{`
+        .cancle {
+          cursor: grab;
+          border: 2px solid;
+        }
+
         div {
           display: flex;
           justify-content: center;
           align-items: center;
+          overflow: visible;
+        }
+
+        .ring {
+          position: relative;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          width: 150px;
+          height: 150px;
+          background: transparent;
+          border: 3px solid #3c3c3c;
+          border-radius: 50%;
+          text-align: center;
+          line-height: 150px;
+          font-family: sans-serif;
+          font-size: 20px;
+          color: black;
+          letter-spacing: 4px;
+          text-transform: uppercase;
+          text-shadow: 0 0 10px white;
+          box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+        }
+        .ring:before {
+          content: "";
+          position: absolute;
+          top: -3px;
+          left: -3px;
+          width: 100%;
+          height: 100%;
+          border: 3px solid transparent;
+          border-top: 3px solid white;
+          border-right: 3px solid white;
+          border-radius: 50%;
+          animation: animateC 2s linear infinite;
+        }
+        span {
+          display: block;
+          position: absolute;
+          top: calc(50% - 2px);
+          left: 50%;
+          width: 50%;
+          height: 4px;
+          background: transparent;
+          transform-origin: left;
+          animation: animate 2s linear infinite;
+        }
+        span:before {
+          content: "";
+          position: absolute;
+          width: 16px;
+          height: 16px;
+          border-radius: 50%;
+          background: #fff000;
+          top: -6px;
+          right: -8px;
+          box-shadow: 0 0 20px #fff000;
+        }
+        @keyframes animateC {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+        @keyframes animate {
+          0% {
+            transform: rotate(45deg);
+          }
+          100% {
+            transform: rotate(405deg);
+          }
         }
       `}</style>
     </div>
