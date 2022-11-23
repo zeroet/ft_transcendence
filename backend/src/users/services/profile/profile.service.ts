89eabd215b99cb010b248571fa7970b96a29f98b
jmcheon <developer.cheon@gmail.com>
2022-11-23 02:40:55 +0100

    refactor: dependencies between user and two factor services

--- backend/src/users/services/profile/profile.service.ts
+++ backend/src/users/services/profile/profile.service.ts
@@ -1,14 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/typeorm';
-import { IUserService } from '../user/user.interface';
 
 @Injectable()
 export class ProfileService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>, // private readonly mailerService: MailerService,
-    @Inject('USER_SERVICE') private userService: IUserService,
   ) {}
 
   async updateUserName(userID: number, newUserName: string) {
