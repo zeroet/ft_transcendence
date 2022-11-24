e1b9a5f733c46de76472d023c656a01dd0fff334
hyungyoo <hjyoo901112@gmail.com>
2022-11-24 06:40:51 +0100

    create : error에 try catch 추가. SWR, fetch에러관리 추가

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+// import { SWRConfig } from "swr";
+// import axios from "axios";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    // <SWRConfig
+    //   value={{
+    //     onError: (error, key) => {
+    //       if (error) {
+    //         // axios.get("/api/auth/refresh");
+    //         console.log("here is _app", error);
+    //       }
+    //     },
+    //   }}
+    // >
+    <Component {...pageProps} />
+    // </SWRConfig>
+  );
 }
