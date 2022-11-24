e157e592fe289c682ddc26f484cc70794f092839
jmcheon <yosoi1225@gmail.com>
2022-11-25 00:20:10 +0100

    update: add interfaces for chat entities to interfaceType.ts of front

--- front/interfaceType.ts
+++ front/interfaceType.ts
@@ -27,3 +27,40 @@ export interface XYType {
   x: number;
   y: number;
 }
+
+export interface IChatroom {
+  chatroom_id: number;
+  owner_id: number;
+  chatroom_name: string;
+  password: string;
+  max_member_num: number;
+  created_at: Date;
+  modified_at: Date;
+}
+
+export interface IChatMember {
+  user_id: number;
+  chatroom_id: number;
+  muted_date: Date;
+  ban_date: Date;
+  created_at: Date;
+  modified_at: Date;
+}
+
+export interface IChatContent {
+  chat_content_id: number;
+  chatroom_id: number;
+  user_id: number;
+  content: string;
+  created_at: Date;
+  modified_at: Date;
+}
+
+export interface IDm {
+  dm_id: number;
+  sender_id: number;
+  receiver_id: number;
+  content: string;
+  created_at: Date;
+  modified_at: Date;
+}
