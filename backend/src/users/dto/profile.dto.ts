6f640d58bc375579a5920392ef59b5942e955bd5
jmcheon <yosoi1225@gmail.com>
2022-11-24 18:20:19 +0100

    fix: profile Dto type from class to interface

--- backend/src/users/dto/profile.dto.ts
+++ backend/src/users/dto/profile.dto.ts
@@ -1,18 +1,4 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
-
-export class profileDTO {
-  @ApiProperty({
-    type: 'string',
-    description: 'username',
-  })
-  @IsString()
+export interface profileDTO {
   username: string;
-
-  @ApiProperty({
-    type: 'string',
-    description: 'image URL',
-  })
-  @IsString()
   image_url: string;
 }
