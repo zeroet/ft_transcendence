a890d355da0aab28dafa044d5b7707c52effa467
keulee <klee8903@gmail.com>
2022-11-25 03:23:47 +0100

    Chat room list component divided in chatroom/dm header and chatroom/dm list

--- front/component/Chat/RoomList.tsx
+++ front/component/Chat/RoomList.tsx
@@ -1,11 +1,15 @@
 import styles from "../../styles/LayoutBox.module.css";
 import ChatRoom from "./RoomList/ChatRoom";
 import DM from "./RoomList/DM";
+import ChatListHeader from "./RoomList/ChatListHeader";
+import DmHeader from "./RoomList/DmHeader";
 
 export default function RoomList() {
   return (
     <div className={styles.box}>
+      <ChatListHeader />
       <ChatRoom />
+      <DmHeader />
       <DM />
     </div>
   );
