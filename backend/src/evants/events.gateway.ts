4bedb8a431ef005e5ce27c16e5709cabe7c3b2c1
jmcheon <developer.cheon@gmail.com>
2022-11-29 00:39:04 +0100

    merge origin/chat into chat

--- backend/src/evants/events.gateway.ts
+++ backend/src/evants/events.gateway.ts
@@ -16,48 +16,17 @@ import { IUser } from 'src/typeorm/interfaces/IUser';
 import { UserService } from 'src/users/services/user/user.service';
 import { User } from 'src/utils/decorators/user.decorator';
 
-<<<<<<< HEAD
-@WebSocketGateway({
-  // namespace: 'chat',
-  cors: process.env.CLINET_URL,
-  // cors: '*',
-})
-export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
-=======
 // @UseGuards(JwtAccessAuthGuard)
 @WebSocketGateway({ path: '/chat', cors: '*' })
 export class EventsGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
   @WebSocketServer() public server: Server;
->>>>>>> 9d3cfbad0fe6d9e28531035e1c02ffc376a722e8
   protected readonly logger = new Logger(EventsGateway.name);
   constructor(
     @Inject('USER_SERVICE') private userService: UserService,
     private readonly jwtAccessStrategy: JwtAccessStrategy,
   ) {}
-
-<<<<<<< HEAD
-  @WebSocketServer() server: Server;
-
-  // @UseGuards(JwtAccessAuthGuard)
-  async handleConnection(@ConnectedSocket() socket: Socket, @User() user) {
-    const currentUser = await this.jwtAccessStrategy.validate(
-      socket.handshake.headers.accesstoken,
-    );
-    console.log('chat socket connected', currentUser);
-
-    // console.log('chat socket connected', user);
-    this.logger.log('user has been connected');
-    // this.logger.log(`Server path:${this.server} connected`);
-  }
-
-  handleDisconnect(socket: Socket) {
-    this.logger.log(`Server path:${this.server.path} disconnected`);
-    console.log('chat socket disconnected');
-  }
-=======
->>>>>>> 9d3cfbad0fe6d9e28531035e1c02ffc376a722e8
 
   @SubscribeMessage('message')
   handleMessage(
@@ -75,24 +44,22 @@ export class EventsGateway
     this.server.emit('dm', socket.id, dmMessage);
   }
 
-  // afterInit(server: any) {
-  //   console.log(`Server path:${this.server.path}`);
-  //   this.logger.log(`Server path:${this.server.path}`);
-  // }
+  afterInit(server: any) {
+    console.log(`Server path:${this.server.path}`);
+    this.logger.log(`Server path:${this.server.path}`);
+  }
 
   @SubscribeMessage('test')
   test(socket: Socket, data: string) {
     socket.on('test', () => console.log('test', data));
   }
-<<<<<<< HEAD
-=======
 
   async handleConnection(socket: Socket) {
     const currentUser = await this.jwtAccessStrategy.validate(
       socket.handshake.headers.accesstoken,
     );
     console.log('chat socket connected', currentUser.id);
-    console.log(`chat cliendt id : ${socket.id}`)
+    console.log(`chat cliendt id : ${socket.id}`);
     this.logger.log('user has been connected');
     this.logger.log(`Server path:${this.server.path} connected`);
   }
@@ -101,5 +68,4 @@ export class EventsGateway
     this.logger.log(`Server path:${this.server.path} disconnected`);
     console.log('chat socket disconnected');
   }
->>>>>>> 9d3cfbad0fe6d9e28531035e1c02ffc376a722e8
 }
