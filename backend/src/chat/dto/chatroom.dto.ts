f4a5dcce061d4df051183223b64bcd2c0a14b7fd
jmcheon <developer.cheon@gmail.com>
2022-11-29 22:32:49 +0100

    add chatroom dto

---
+++ backend/src/chat/dto/chatroom.dto.ts
@@ -0,0 +1,39 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ChatroomDto {
+  @ApiProperty({
+    type: 'number',
+    description: 'chatroom id',
+  })
+  chatroomId: number;
+
+  @ApiProperty({
+    type: 'number',
+    description: 'owner id',
+  })
+  ownerId: number;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'chatroom name',
+  })
+  chatroomName: string;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'password',
+  })
+  password: string;
+
+  @ApiProperty({
+    type: 'timestamp',
+    description: 'created time',
+  })
+  createdAt: Date;
+
+  @ApiProperty({
+    type: 'timestamp',
+    description: 'modified time',
+  })
+  modifedAt: Date;
+}
