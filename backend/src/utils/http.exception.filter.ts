ab96e9de8c03bdcbbd53f784edcc63a88200e58d
jmcheon <developer.cheon@gmail.com>
2022-11-29 22:32:26 +0100

    add: http exception filter

---
+++ backend/src/utils/http.exception.filter.ts
@@ -0,0 +1,27 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const status = exception.getStatus();
+    const err = exception.getResponse() as
+      | string
+      | { error: string; statusCode: 400; message: string[] };
+    //   if (typeof err !== 'string' && err.error === 'Bad Requeset') {
+    //     return response.status(status).json({
+    //         success: false,
+    //         code: status,
+
+    //     })
+    //   }
+    console.log(status, err);
+  }
+}
