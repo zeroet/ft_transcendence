ac0f520b22a80908cf8ade957faf79c130cef1ca
jmcheon <developer.cheon@gmail.com>
2022-12-02 21:16:41 +0100

    update: members routes for dm

--- backend/src/chat/controllors/dm/dm.controller.ts
+++ backend/src/chat/controllors/dm/dm.controller.ts
@@ -7,12 +7,21 @@ import { IDmService } from 'src/chat/services/dm/dm.interface';
 export class DmController {
   constructor(@Inject('DM_SERVICE') private dmSerivce: IDmService) {}
   @ApiOperation({
+    summary: 'Get members for a DM / 특정 디엠의 참여자 가져오기',
+  })
+  @Get(':id/contents')
+  getMembers() {}
+
+  @ApiOperation({ summary: 'post members / 특정 디엠에 참여자 추가하기' })
+  @Post(':id/contents')
+  postMembers() {}
+  @ApiOperation({
     summary: 'Get contents for a DM / 특정 디엠의 대화내용 가져오기',
   })
   @Get(':id/contents')
-  getMessages() {}
+  getContents() {}
 
   @ApiOperation({ summary: 'post contents / 특정 디엠에 대화내용 입력하기' })
   @Post(':id/contents')
-  postMessage() {}
+  postContents() {}
 }
