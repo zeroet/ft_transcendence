6fed882d83959a92e1a14a5c05e9f8521bad2be2
hyungyoo <hjyoo901112@gmail.com>
2022-12-03 18:51:13 +0100

    udpate  game

--- front/component/Game/GameList.tsx
+++ front/component/Game/GameList.tsx
@@ -7,17 +7,15 @@ import useSocket from "../Utils/socket";
 export default function GameList({ accessToken }: { accessToken: string }) {
   const [socket] = useSocket(accessToken, "game");
   const [roomList, setRoomList] = useState<string[]>([]);
- 
 
   /**
-   * 
+   *
    * room_list로 목록을 받으면,
    * roomList안에 리스트를 배열로
    * 배열후에, router.push()를 이용하여
    * query안에 myRole은 watcher이다.
-   * 
+   *
    */
-ㄴ
   useEffect((): (() => void) => {
     console.log(`socket on ${socket?.id}`);
     socket?.emit("room-list", "");
