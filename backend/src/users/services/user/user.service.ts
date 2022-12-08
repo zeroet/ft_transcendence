4c951761fc00b6f868fc622b0c55884fd964472e
jmcheon <yosoi1225@gmail.com>
2022-12-08 09:05:30 +0100

    update: users/all api to return without block list

--- backend/src/users/services/user/user.service.ts
+++ backend/src/users/services/user/user.service.ts
@@ -24,7 +24,6 @@ export class UserService implements IUserService {
       .where('users.user_id=:userId', { userId })
       .getOne();
     if (!user) {
-      // console.log(`findUserByIdOrFail of id: ${userId} not found,`);
       throw new NotFoundException(`User of id:${userId} not found`);
     }
     return user;
@@ -46,40 +45,20 @@ export class UserService implements IUserService {
       .where('users.user_id=:id', { id })
       .getOne();
     return user;
-    // return await this.userRepository.findOneBy({ id: id });
   }
   async getUserById(id: number) {
     this.logger.debug(`getUserById() id: ${id}`);
-    if (id !== undefined) {
-      // const user = await this.userRepository.findOneBy({ id: id });
-      // const user = await this.userRepository.findOne({ where: { id } });
-      const user = await this.userRepository
-        .createQueryBuilder('users')
-        .leftJoinAndSelect(
-          'users.Block',
-          'block',
-          'users.user_id = block.user_id',
-        )
-        .where('users.user_id=:id', { id })
-        .getOne();
-      // console.log('getUserById() user:', user);
-      // this.logger.debug('getUserById() user:');
-      // console.log(user);
-      // console.log('getuserbyid:', user);
-      // if (!user) throw new NotFoundException(`User by #id ${id} not found`);
-      return user;
-    }
+    const user = await this.userRepository
+      .createQueryBuilder('users')
+      .where('users.user_id=:id', { id })
+      .getOne();
+    return user;
   }
 
   async getAllUsers() {
     // const users = await this.userRepository.find();
     const users = await this.userRepository
       .createQueryBuilder('users')
-      // .leftJoinAndSelect(
-      //   'users.Block',
-      //   'block',
-      //   'users.user_id = block.user_id',
-      // )
       .getMany();
     // console.log('users info:', users);
     if (users) return users;
@@ -121,13 +100,6 @@ export class UserService implements IUserService {
       );
     }
     const removedBlock = await this.blockRepository.remove(block);
-    // const removedBlock = this.blockRepository
-    //   .createQueryBuilder('block')
-    //   .delete()
-    //   .from(Block)
-    //   .where('user_id=:userId', { userId })
-    //   .andWhere('blocked_user_id=:unBlockUserId', { unBlockUserId })
-    //   .execute();
     console.log('removed block:', removedBlock);
     return removedBlock;
   }
