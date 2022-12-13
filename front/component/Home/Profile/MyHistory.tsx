28e5340bcd4302287fa07cd09de3d5190dc579d5
hyungyoo <hjyoo901112@gmail.com>
2022-12-13 23:05:05 +0100

    update : add css history of match

--- front/component/Home/Profile/MyHistory.tsx
+++ front/component/Home/Profile/MyHistory.tsx
@@ -12,12 +12,7 @@ const MyHistory = ({ id }: { id: number }) => {
   useEffect(() => {
     mutate(`/api/users/match/${id}`);
   }, [userData]);
-  /**
-   * loser.username
-   * winner.username
-   * score : [number, number]
-   *
-   */
+
   console.log(historyData);
   if (!historyData || !userData) return <Loading />;
   return (
@@ -25,37 +20,31 @@ const MyHistory = ({ id }: { id: number }) => {
       <div className="dummy box"></div>
       <div className="history box">
         <h1>LAST 5 MATCHES HISTORY</h1>
-        <div className="history-div">
-          {historyData &&
-            userData &&
-            historyData.map((eachHistory: any) => {
-              return (
-                <div key={eachHistory.id}>
-                  <EachHistory
-                    winer={eachHistory.winner.username}
-                    loser={eachHistory.loser.username}
-                    winnerScore={
-                      eachHistory.score[0] > eachHistory.score[1]
-                        ? eachHistory.score[0]
-                        : eachHistory.score[1]
-                    }
-                    loserSocre={
-                      eachHistory.score[0] < eachHistory.score[1]
-                        ? eachHistory.score[0]
-                        : eachHistory.score[1]
-                    }
-                    username={userData.username}
-                  />
-                </div>
-              );
-            })}
-        </div>
+        {historyData &&
+          userData &&
+          historyData.map((eachHistory: any) => {
+            return (
+              <div key={eachHistory.id} className="history">
+                <EachHistory
+                  winer={eachHistory.winner.username}
+                  loser={eachHistory.loser.username}
+                  winnerScore={
+                    eachHistory.score[0] > eachHistory.score[1]
+                      ? eachHistory.score[0]
+                      : eachHistory.score[1]
+                  }
+                  loserSocre={
+                    eachHistory.score[0] < eachHistory.score[1]
+                      ? eachHistory.score[0]
+                      : eachHistory.score[1]
+                  }
+                  username={userData.username}
+                />
+              </div>
+            );
+          })}
       </div>
       <style jsx>{`
-        .history-div {
-          display: flex;
-          flex-direction: column;
-        }
         div {
           display: grid;
           grid-template-columns: 2fr 4fr;
