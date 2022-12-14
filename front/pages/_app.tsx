67dcf5ba41fb881d37f5da441bc07fae470ba08b
zeroet <y36609672@gmail.com>
2022-12-14 20:28:58 +0100

    Pq

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -16,10 +16,10 @@ export default function App({ Component, pageProps }: AppProps) {
   const { data: myData } = useSWR("/api/users");
 
   useEffect(() => {
-    gameSocket?.on("createQ", () => {
+    gameSocket?.on("createQ", (senderId:number) => {
       const response = confirm("get test siginal");
       if (response) {
-        gameSocket?.emit("Private", myData.id);
+        gameSocket?.emit("Private", senderId);
       }
     });
 
