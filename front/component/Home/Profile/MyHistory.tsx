54ae1e0c6adb96926b27eae12a01b58745432b45
Keungeun LEE <klee8903@gmail.com>
2022-12-15 08:23:32 +0100

    MyHistory part commented one

--- front/component/Home/Profile/MyHistory.tsx
+++ front/component/Home/Profile/MyHistory.tsx
@@ -19,6 +19,11 @@ const MyHistory = ({ id }: { id: number }) => {
       <div className="dummy box"></div>
       <div className="history">
         <h1>LAST 5 MATCHES</h1>
+        {/* <div className="startmsg">
+          <p>You don't have match history yet</p>
+          <br />
+          <p>Go play pong and make some history!</p>
+        </div> */}
         {historyData &&
           userData &&
           historyData.map((eachHistory: any) => {
@@ -44,6 +49,14 @@ const MyHistory = ({ id }: { id: number }) => {
           })}
       </div>
       <style jsx>{`
+        .startmsg {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          font-size: 20px;
+          padding-top: 50px;
+          font-weight: bold;
+        }
         div {
           display: grid;
           grid-template-columns: 2fr 4fr;
